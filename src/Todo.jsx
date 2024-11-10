f4a2ea20437734b0599be86e624c31bdd12a2dc0
jobeda <jobedakhatun710@gmail.com>
2024-11-10 23:49:12 +0600

    conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,11 +4,30 @@
 //         <li>task : {task}</li>
 //     )
 // }
+//conditional rendering 1
+// export default function Todo({ task, isDone }) {
+//     if (isDone) {
+//         return <li>Finished task : {task}</li>
+//     } else {
+//         return <li>Do task : {task}</li>
+//     }
+// }
+
+//conditional rendering 2
+// export default function Todo({ task, isDone }) {
+
+//     return (
+//         <li>{isDone ? 'finished' : 'work on '} {task}</li>
+//     )
+
+// }
+//conditional rendering 3
+
 export default function Todo({ task, isDone }) {
-    if (isDone) {
-        return <li>Finished task : {task}</li>
-    } else {
-        return <li>Do task : {task}</li>
-    }
+
+    return (
+        // <li>{task} {isDone && 'done'} </li>
+        <li>{task} {isDone || 'work on'} </li>
+    )
 
 }
